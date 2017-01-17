9136f870386a27a5981c77996cda0b2fe1a681fc
Henrique Zavareze <henrique@zavareze.com.br>
2017-01-17 12:31:26 -0200

    athlete routing info

--- src/components/Athletes/AthleteInner.js
+++ src/components/Athletes/AthleteInner.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import athletes from 'data/athletes';
 
 class AthleteInner extends React.Component {
 	render () {
 
 		const { id } = this.props.params;
 
+		const ourSoccerStar = athletes.filter((athlete) => athlete.id === id)[0];
+
 		return (
-			<div>Hello, {id}</div>
+			<div>Hello, {ourSoccerStar.known_as}</div>
 		);
 	}
 }
