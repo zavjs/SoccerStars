472af574683452afcff11bb1596d9b2b4b30960d
Henrique Zavareze <henrique@zavareze.com.br>
2017-01-20 09:46:26 -0200

    athlete inner component

--- src/components/Athletes/AthleteInner.js
+++ src/components/Athletes/AthleteInner.js
@@ -3,8 +3,8 @@ import athletes from 'data/athletes';
 import { Link } from 'react-router';
 
 class AthleteInner extends React.Component {
-	render () {
 
+	render () {
 		const { id } = this.props.params;
 
 		const ourSoccerStar = athletes.filter((athlete) => athlete.id === id)[0];
@@ -13,35 +13,56 @@ class AthleteInner extends React.Component {
 
 		return (
 			<div className="athlete-inner">
-				<header>
-					<div style={{ backgroundImage: bgImage }} className="cover">
-						<figure className="athlete-profile">
-							<img 
-								src={'/public/img/' + ourSoccerStar.image } />
-						</figure>
-						<h2 className="athlete-profile-name text-center">{ourSoccerStar.known_as}</h2>
-					</div>
-					<div className="athlete-tabs">
-						<div className="athlete-tab text-center inline-block inactive-tab">
+				<article>
+					<header>
+						<div style={{ backgroundImage: bgImage }} className="cover">
+							<figure className="athlete-profile">
+								<img 
+									src={'/public/img/' + ourSoccerStar.image } />
+							</figure>
+							<h2 className="athlete-profile-name text-center">{ourSoccerStar.known_as}</h2>
+						</div>
+						<div className="athlete-tabs">
+							<div className="athlete-tab text-center inline-block inactive-tab">
 
+							</div>
+							<div className="athlete-tab text-center inline-block active">
+								<Link to={`/athlete/${ourSoccerStar.id}/main`}>Main</Link>
+							</div>
+							<div className="athlete-tab text-center inline-block ">
+								<Link to={`/athlete/${ourSoccerStar.id}/chat`}>Chat</Link>
+							</div>
+							<div className="athlete-tab text-center inline-block ">
+								<Link to={`/athlete/${ourSoccerStar.id}/photos`}>Photos</Link>
+							</div>
+							<div className="athlete-tab text-center inline-block ">
+								<Link to={`/athlete/${ourSoccerStar.id}/videos`}>Videos</Link>
+							</div>
 						</div>
-						<div className="athlete-tab text-center inline-block active">
-							<Link to={`/athlete/${ourSoccerStar.id}/main`}>Main</Link>
+					</header>
+					<div className="athlete-content">
+						<div className="athlete-info">
+							<p className="abstract">
+								{ourSoccerStar.description}
+							</p>
+							<i className="fa fa-info" />
 						</div>
-						<div className="athlete-tab text-center inline-block ">
-							<Link to={`/athlete/${ourSoccerStar.id}/chat`}>Chat</Link>
-						</div>
-						<div className="athlete-tab text-center inline-block ">
-							<Link to={`/athlete/${ourSoccerStar.id}/photos`}>Photos</Link>
-						</div>
-						<div className="athlete-tab text-center inline-block ">
-							<Link to={`/athlete/${ourSoccerStar.id}/videos`}>Videos</Link>
+						<div className="twitter-feed">
+							<p className="twitter-status">
+								<a href="twitter.com/charleston">@charlestonfan</a>: Tonight victory was amazing.
+								Aubameyang scored thrice! #aubameyang
+							</p>
+							<p className="twitter-status">
+								<a href="twitter.com/gazelle98">@gazelle98</a>: I hope 
+								@aubameyang plays forever on borussia.
+							</p>
+							<p className="twitter-status">
+								<a href="twitter.com/leopardine22">@leopardine22</a>: Tonight victory was amazing.
+								Aubameyang scored thrice! #aubameyang
+							</p>
 						</div>
 					</div>
-				</header>
-				<div className="athlete-content">
-
-				</div>
+				</article>
 			</div>
 		);
 	}
